37ea775b5532c713d7c925d6b9f5f4b4ab9d7f62
SIHTAM <56382532+SIHTAM@users.noreply.github.com>
2021-12-01 11:45:08 +0100

    Added attributes of the different elements in the view.

--- app/src/main/java/com/example/myteo/MainActivity.java
+++ app/src/main/java/com/example/myteo/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.myteo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV, iconIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
